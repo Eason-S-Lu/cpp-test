643659fb3abee1c7d3363598d73abe613c5206b7
Eason Lu <github@easonlu.ml>
2022-10-12 09:03:45 -0700

    add int to program

--- test.cpp
+++ test.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World! \n";
-  cout << "You may need g++ \n";
-  // this is a comment
-  return 0;
+	cout << "Hello World! \n";
+	cout << "You may need g++ \n";
+		// this is a comment
+		/* This is a comment that can last
+		more than one lines.*/
+	int a;
+	a = 1;
+	cout << a;
+return 0;
 }
